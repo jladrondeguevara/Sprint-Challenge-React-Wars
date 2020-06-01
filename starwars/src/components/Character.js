cf9dba32050ace8c4d087611710f864f8f12fbd3
Jeovanni <jeovannildg@gmail.com>
2020-05-31 20:22:54 -0600

    created fetch method

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,9 +1,20 @@
 // Write your Character component here
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Character = () => {
+    const [characterData, setCharacterData] = useState([]);
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/character/')
+        .then(characters => {
+            console.log(characters.data.results);
+        })
+    }, [])
+
     return (
-        console.log("hello")
+        <div>
+            <h1>Character</h1>
+        </div>
     );
 };
 
